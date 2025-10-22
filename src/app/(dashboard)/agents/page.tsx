564916cf6a988972e6b5c08122396ac51e8800fb
codabu-dev <hello@codabu.com>
2025-10-22 19:06:40 +0200

    feat: setup agent form and protected procedure

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,3 +1,5 @@
+import { auth } from '@/lib/auth';
+import { AgentsListHeader } from '@/modules/agents/ui/components/agents-list-header';
 import {
   AgentsView,
   AgentsViewErorr,
@@ -5,20 +7,33 @@ import {
 } from '@/modules/agents/ui/views/agents-view';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { ErrorBoundary } from 'react-error-boundary';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    redirect('/sign-in');
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<AgentsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewErorr />} />
-        <AgentsView />
-      </Suspense>
-    </HydrationBoundary>
+    <>
+      <AgentsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<AgentsViewLoading />}>
+          <ErrorBoundary fallback={<AgentsViewErorr />} />
+          <AgentsView />
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,31 @@
-import { createTRPCRouter, baseProcedure } from '@/trpc/init';
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
 import { db } from '@/db';
 import { agent } from '@/db/schema';
+import { agentsInsertSchema } from '../schemas';
+import z from 'zod';
+import { eq } from 'drizzle-orm';
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agent)
+        .where(eq(agent.id, input.id));
+      return existingAgent;
+    }),
+  getMany: protectedProcedure.query(async () => {
     return await db.select().from(agent);
-  })
+  }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agent)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning();
+
+      return createdAgent;
+    })
 });
